0c15cd92b7bf15a9999588d4f2921dede905f9cf
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-04-08 14:37:00 +0200

    initialized android project and added static main view

---
+++ app/src/main/java/com/example/moonphases/FullMoonList.kt
@@ -0,0 +1,12 @@
+package com.example.moonphases
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FullMoonList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_moon_list)
+    }
+}
