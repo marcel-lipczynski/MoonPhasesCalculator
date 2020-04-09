51d6d541379c407fffc29f3aa0fe785bc6e1a03c
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-04-09 21:51:06 +0200

    Implemented dynamic ListView and new button in full_moon_list_activity

--- app/src/main/java/com/example/moonphases/FullMoonList.kt
+++ app/src/main/java/com/example/moonphases/FullMoonList.kt
@@ -2,10 +2,19 @@ package com.example.moonphases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_full_moon_list.*
 
 class FullMoonList : AppCompatActivity() {
 
     private var usedAlgorithm: String = "Trig1"
+    private var startingYear = "1900"
+    private var listItems = arrayListOf<String>()
+    private lateinit var adapter: ArrayAdapter<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +22,43 @@ class FullMoonList : AppCompatActivity() {
 
         val extras = intent.extras ?: return
         usedAlgorithm = extras.getString("algorithm").toString()
+        val currentlyUsedAlgorithm = "Currently used algorithm: $usedAlgorithm"
+        algorithmInfo.text = currentlyUsedAlgorithm
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        fullMoonList.adapter = adapter
+    }
 
+    fun onClickAddYearButton(view: View) {
+
+        val currentValue = yearInput.text.toString()
+        if (currentValue == "") {
+            yearInput.setText(startingYear)
+        } else if (Integer.parseInt(currentValue) == 2200) {
+            return
+        } else {
+            yearInput.setText((Integer.parseInt(currentValue) + 1).toString())
+        }
+
+    }
+
+    fun onClickSubtractYearButton(view: View) {
+
+        val currentValue = yearInput.text.toString()
+        if (currentValue == "") {
+            yearInput.setText(startingYear)
+        } else if (Integer.parseInt(currentValue) == 1900) {
+            return
+        } else {
+            yearInput.setText((Integer.parseInt(currentValue) - 1).toString())
+
+        }
+
+    }
+
+    fun addItem(view: View){
+        
+        listItems.add(startingYear)
+        adapter.notifyDataSetChanged()
     }
 
 
